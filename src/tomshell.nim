c14be5273d3de15267fc3fec74fe5695e07b6842
ateRstones <8556589+ateRstones@users.noreply.github.com>
2024-04-05 22:41:58 +0200

    Adding features

--- src/tomshell.nim
+++ src/tomshell.nim
@@ -1,15 +1,39 @@
 import os
 import strutils
+import terminal
+
+type
+  Feature = enum
+    git
+
+proc handleLiveInput(): string =
+  var ret = false
+  var value = ""
+  while not ret:
+    var print = true
+    var key = getch()
+    if key == '\n':
+      ret = true
+
+    if print:
+      stdout.write (int)key
+
+  value
 
 when isMainModule:
   var command: string = ""
   var debugMode = false
   var execute = true
+  var featureMap: array[Feature, bool]
+
+  for f in featureMap.mitems:
+    f = true
 
   while true:
     execute = true
-    stdout.write "T> "
-    command = readLine(stdin)
+    stdout.styledWrite(fgBlue, "T> ")
+    #stdout.write "T> "
+    command = handleLiveInput()
 
     if command == "exit":
       break
@@ -18,12 +42,17 @@ when isMainModule:
       debugMode = not debugMode
       execute = false
 
-    elif command == "gs":
-      command = "git status"
+    if command == "toggle-git-mode":
+      featureMap[git] = not featureMap[git]
+      execute = false
 
-    elif command.startsWith("gcam"):
-      command = command.replace("gcam ", "git commit -am \"")
-      command.add("\"")
+    if featureMap[git]:
+      if command == "gs":
+        command = "git status"
+
+      elif command.startsWith("gcam"):
+        command = command.replace("gcam ", "git commit -am \"")
+        command.add("\"")
 
     if execute:
       if debugMode:
